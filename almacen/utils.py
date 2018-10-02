0c43ed5310d1f28a22fcfc9872f046fd378c8bef
ramirovaz <r.vazquez@meditecsols.com>
2018-10-02 08:15:50 -0500

    better sintax, clean code

--- almacen/utils.py
+++ almacen/utils.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 def dame_token():
   context = {}
-  url = settings.URL_API_AUTHENTICATE
+  url = settings.URL_BASE_API + settings.URL_API_AUTHENTICATE
   response = requests.post(url, data={'db':settings.CONNECT_DB, 'login':settings.CONNECT_LOGIN, 'password':settings.CONNECT_PASSWORD})
 
   if response.ok:
@@ -16,5 +16,55 @@ def dame_token():
   else:
     print("error en request")
 
-  print(context)
+  #print(context)
   return context
+
+
+def dame_movimientos(token):
+  context = {}
+  url = settings.URL_BASE_API + "/read/stock.move.line/"
+  response = requests.post(url, data={'token':token})
+
+  if response.ok:
+    print("request OK ...")
+    response_json = response.json()
+    try:
+      context['movimientos'] = response_json
+    except KeyError:
+      context['movimientos'] = None
+  else:
+    print("error en request")
+
+  #print(context)
+  return context
+
+
+def dame_picking(picking_id, token):
+  url = settings.URL_BASE_API + "/read/stock.picking/{}".format(picking_id)
+  response = requests.post(url, data={'token':token})
+  if response.ok:
+    return response.json()
+  else:
+    return response
+
+
+
+def agrega_picking(diccionario, token):
+  diccionario_new = {"movimientos": []}
+  if 'movimientos' in diccionario.keys():
+    for x in diccionario['movimientos']:
+      if x['picking_id']:
+        picking_id_post = x['picking_id'][0]
+        response_json = dame_picking(picking_id_post, token)
+        x['response_picking'] = response_json
+        #print("--------------------------INI")
+        #print(x)
+        #print("--------------------------FIN")
+        diccionario_new['movimientos'].append(x)
+
+      else:
+        #print("--------------------------INI----////////")
+        #print(x)
+        #print("--------------------------FIN----////////")
+        diccionario_new['movimientos'].append(x)
+  return diccionario_new 
--- almacen/views.py
+++ almacen/views.py
@@ -1,11 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from almacen.utils import dame_token
+from almacen.utils import dame_token, dame_movimientos, agrega_picking
 
 def index(request):
     #latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {}
-    context["test"] = "ping"
-    context["token"] = dame_token()
+    context["resp_token"] = dame_token()
+    token = context["resp_token"]["token"]
+    movimientos = dame_movimientos(token)
+    context["resp_movimientos"] = agrega_picking(movimientos, token)
+
 
     return render(request, 'almacen/index.html', context)
